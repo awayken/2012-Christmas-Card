8141a26d2aeffd996fbd7c3dc992c85895cc7123
Miles Rausch <miles@milesrausch.com>
2012-12-16 22:13:37 -0600

    Began work on JS spec with Jasmine. I should probably start planning some of this stuff out instead of just diving in and writing code, but at least I'm specking before I wrote the code. That has to count for something.

--- _tests/spec/main.js
+++ _tests/spec/main.js
@@ -1,3 +1,31 @@
 describe("Main", function() {
+	var marker;
+
+	beforeEach(function() {
+		marker = new Marker();
+	});
+
+	it("should have a marker", function() {
+		expect( Marker ).toBeDefined();
+		expect( marker ).toBeDefined();
+	});
+
+    describe("Marker", function() {
+
+        it('should have a position', function() {
+            expect( marker.setPosition ).toBeDefined();
+            expect( marker.getPosition ).toBeDefined();
+        });
+
+        it('should have getter', function() {
+            expect( marker.getPosition() ).toEqual( [ 0, 0 ] );
+        });
+
+        it('should have setter', function() {
+            marker.setPosition( 2, 4 );
+            expect( marker.getPosition() ).toEqual( [ 2, 4 ] );
+        });
+
+    });
 
 });
--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-
+var Marker = function() {
+	// this.property
+	this.position = [ 0, 0 ];
+}
+Marker.prototype.getPosition = function() {
+	return this.position;
+}
+Marker.prototype.setPosition = function( rowIndex, boxIndex ) {
+	this.position = [ rowIndex, boxIndex ];
+}
