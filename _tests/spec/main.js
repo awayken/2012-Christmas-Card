cfa89828572b6141cdf40f8fe2c8ed2935f4cb78
awayken <miles@milesrausch.com>
2012-12-18 22:48:06 -0600

    Added moveTo method to Marker(). This does some simple node manipulation, which I haven't played with in a while. Will have to do some testing on other browsers.

--- _tests/spec/main.js
+++ _tests/spec/main.js
@@ -26,6 +26,29 @@ describe("Main", function() {
             expect( marker.getPosition() ).toEqual( [ 2, 4 ] );
         });
 
+        it('should be able to move', function () {
+            expect( marker.moveTo ).toBeDefined();
+        });
+
+        it('should move to a known row and box', function () {
+            row = document.createElement('div');
+            row.id = 'row1';
+            box1 = document.createElement('div');
+            box2 = document.createElement('div');
+            row.appendChild( box1 );
+            row.appendChild( box2 );
+            document.body.appendChild( row );
+
+            expect( box1.childNodes.length ).toEqual( 0 );
+            marker.moveTo( 0, 0 );
+            expect( box1.childNodes.length ).toEqual( 1 );
+            expect( box1.childNodes[ 0 ] ).toBe( marker.node );
+            marker.moveTo( 0, 1 );
+            expect( box1.childNodes.length ).toEqual( 0 );
+            expect( box2.childNodes.length ).toEqual( 1 );
+            expect( box2.childNodes[ 0 ] ).toBe( marker.node );
+        });
+
     });
 
 });
--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 var Marker = function() {
-	// this.property
 	this.position = [ 0, 0 ];
+	this.node = document.createElement('div');
+	this.node.id = 'marker';
+	this.node.innerHTML = '&#9731;';
 }
 Marker.prototype.getPosition = function() {
 	return this.position;
 }
 Marker.prototype.setPosition = function( rowIndex, boxIndex ) {
 	this.position = [ rowIndex, boxIndex ];
+}
+Marker.prototype.moveTo = function( rowIndex, boxIndex ) {
+	var box = document.getElementById('row' + ( rowIndex + 1 ) ).childNodes[ boxIndex ];
+	box.appendChild( this.node );
+	this.setPosition( rowIndex, boxIndex );
 }
