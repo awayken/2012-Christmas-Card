78df02858d7821f961a5ab9cafb4c128e38fef72
Miles Rausch <miles@milesrausch.com>
2012-12-23 00:48:10 -0600

    Added die and card objects. These control the die and the life cards, respectively. Both have some sort of DOM manipulation to visually represent how they affect things. In both cases, I let CSS do the heavy lifting, and I can mostly just tweak class attributes.

--- _tests/spec/main.js
+++ _tests/spec/main.js
@@ -1,22 +1,16 @@
 describe('Game', function() {
-	var marker;
-
-	beforeEach(function() {
-		marker = new Marker();
-        die = new Die();
-	});
-
-	it("should have a marker", function() {
-		expect( Marker ).toBeDefined();
-		expect( marker ).toBeDefined();
-	});
-
-    it('should have a die', function() {
-        expect( Die ).toBeDefined();
-        expect( die ).toBeDefined();
-    })
 
     describe('marker', function() {
+        var marker;
+
+        beforeEach(function() {
+            marker = new Marker();
+        });
+
+        it("should exist", function() {
+            expect( Marker ).toBeDefined();
+            expect( marker ).toBeDefined();
+        });
 
         it('should have a position', function() {
             expect( marker.setPosition ).toBeDefined();
@@ -35,12 +29,12 @@ describe('Game', function() {
         describe('should move', function () {
 
             beforeEach(function () {
-                row = document.createElement('div');
-                row.id = 'row1';
+                row = document.createElement('ol');
+                row.id = 'row_1';
 
-                box1 = document.createElement('div');
-                box2 = document.createElement('div');
-                box3 = document.createElement('div');
+                box1 = document.createElement('li');
+                box2 = document.createElement('li');
+                box3 = document.createElement('li');
 
                 row.appendChild( box1 );
                 row.appendChild( box2 );
@@ -56,7 +50,7 @@ describe('Game', function() {
             it('to a known row and box', function () {
                 expect( marker.moveTo ).toBeDefined();
                 expect( box1.childNodes.length ).toEqual( 0 );
-                marker.moveTo( 0, 0 );
+                marker.setup();
                 expect( box1.childNodes.length ).toEqual( 1 );
                 expect( box1.childNodes[ 0 ] ).toBe( marker.node );
                 marker.moveTo( 0, 1 );
@@ -67,7 +61,7 @@ describe('Game', function() {
 
             it('by a number of spaces', function () {
                 expect( marker.move ).toBeDefined();
-                marker.moveTo( 0, 0 );
+                marker.setup();
                 expect( box1.childNodes.length ).toEqual( 1 );
                 expect( box1.childNodes[ 0 ] ).toBe( marker.node );
                 marker.move( 2 );
@@ -85,6 +79,16 @@ describe('Game', function() {
     });
 
     describe('die', function() {
+        var die;
+
+        beforeEach(function() {
+            die = new Die();
+        });
+
+        it('should exist', function() {
+            expect( Die ).toBeDefined();
+            expect( die ).toBeDefined();
+        });
 
         it('should have a roll', function() {
             expect( die.getRoll ).toBeDefined();
@@ -98,6 +102,94 @@ describe('Game', function() {
         it('should have setter', function() {
             die.setRoll( 4 );
             expect( die.getRoll() ).toEqual( 4 );
+        });
+
+        describe('should roll', function () {
+
+            beforeEach(function() {
+                dice = document.createElement('div');
+                dice.id = 'dice';
+                document.body.appendChild( dice );
+            });
+
+            afterEach(function() {
+                document.body.removeChild( dice );
+            });
+
+            it('and set the roll value', function () {
+                expect( die.roll ).toBeDefined();
+                expect( die.getRoll() ).toEqual( 0 );
+                die.roll();
+                expect( die.getRoll() ).not.toEqual( 0 );
+            });
+
+            it('and display that roll value', function() {
+                expect( die.node.className ).toEqual( 'side_0' );
+                die.roll();
+                expect( die.node.className ).not.toEqual( 'side_0' );
+                expect( die.node.className ).toEqual( 'side_' + die.getRoll() );
+            });
+
+        });
+
+    });
+
+    describe('card', function() {
+        var card;
+
+        beforeEach(function() {
+            card = new Card();
+        });
+
+        it('should exist', function() {
+            expect( Card ).toBeDefined();
+            expect( card ).toBeDefined();
+        });
+
+        it('should have a currentCard', function() {
+            expect( card.getCurrentCard ).toBeDefined();
+            expect( card.setCurrentCard ).toBeDefined();
+        });
+
+        it('should have getter', function() {
+            expect( card.getCurrentCard() ).toEqual( 0 );
+        });
+
+        it('should have setter', function() {
+            card.setCurrentCard( 2 );
+            expect( card.getCurrentCard() ).toEqual( 2 );
+        });
+
+        describe('should draw', function () {
+
+            beforeEach(function() {
+                life_cards = document.createElement('ul');
+                life_cards.id = 'life-card-list';
+
+                card1 = document.createElement('li');
+                card2 = document.createElement('li');
+                card3 = document.createElement('li');
+
+                life_cards.appendChild( card1 );
+                life_cards.appendChild( card2 );
+                life_cards.appendChild( card3 );
+
+                document.body.appendChild( life_cards );
+
+                card.setup();
+            });
+
+            afterEach(function() {
+                document.body.removeChild( life_cards );
+            });
+
+            it('and display that card', function() {
+                var card_node;
+                card.draw();
+                card_node = document.getElementById('life-card-list').getElementsByTagName('li')[ card.getCurrentCard() ];
+                expect( card_node.className ).toContain( 'drawn' );
+            });
+
         });
 
     });
--- js/main.js
+++ js/main.js
@@ -16,7 +16,9 @@ var Marker = function() {
     // The actual DOM node that represents the marker.
 	this.node = document.createElement('div');
 	this.node.id = 'marker';
-	this.node.innerHTML = '&#9731;';
+    this.node.innerHTML = '&#9731;';
+
+    this.setup();
 }
 // Function: getPosition
 // Getter for <position>
@@ -28,23 +30,41 @@ Marker.prototype.getPosition = function() {
 Marker.prototype.setPosition = function( rowIndex, boxIndex ) {
 	this.position = [ rowIndex, boxIndex ];
 }
+// Function: setup
+// Contains the setup actions.
+Marker.prototype.setup = function() {
+    var row = document.getElementById('row_1'),
+        box;
+
+    if ( row ) {
+        box = row.getElementsByTagName('li')[ 0 ];
+    }
+
+    if ( box ) {
+        box.appendChild( this.node );
+    }
+}
 // Function: moveTo
 // Moves the Marker to a specific position as row, box
 Marker.prototype.moveTo = function( rowIndex, boxIndex ) {
     var currentPosition = this.getPosition(),
         oldBox,
         newBox,
-        oldRow = document.getElementById('row' + currentPosition[ 0 ] ),
-	    newRow = document.getElementById('row' + ( rowIndex + 1 ) );
+        oldRow = document.getElementById('row_' + ( currentPosition[ 0 ] + 1 ) ),
+	    newRow = document.getElementById('row_' + ( rowIndex + 1 ) );
 
-    if ( oldRow != null ) {
-        oldBox = oldRow.childNodes[ currentPosition[ 1 ] ];
-        oldBox.removeChild( this.node );
+    if ( oldRow ) {
+        oldBox = oldRow.getElementsByTagName('li')[ currentPosition[ 1 ] ];
+        if ( oldBox ) {
+            oldBox.removeChild( this.node );
+        }
     }
-    if ( newRow != null ) {
-        newBox = newRow.childNodes[ boxIndex ];
-        newBox.appendChild( this.node );
-        this.setPosition( rowIndex, boxIndex );
+    if ( newRow ) {
+        newBox = newRow.getElementsByTagName('li')[ boxIndex ];
+        if ( newBox ) {
+            newBox.appendChild( this.node );
+            this.setPosition( rowIndex, boxIndex );
+        }
     }
 }
 // Function: move
@@ -59,17 +79,89 @@ Marker.prototype.move = function( spaces ) {
     Defines the Die object.
 ******************************************************************************/
 var Die = function() {
+    var dice = document.getElementById('dice');
+
     // Variable: roll
     // Tracks the current die roll
-    this.roll = 0;
+    this.rollValue = 0;
+
+    // Variable: node
+    // The actual DOM node that represents the die.
+    this.node = document.createElement('div');
+    this.node.id = 'die';
+    this.node.className = 'side_0';
+    this.node.innerHTML = '<span id="dot_1">&bull;</span><span id="dot_2">&bull;</span><span id="dot_3">&bull;</span><span id="dot_4">&bull;</span><span id="dot_5">&bull;</span><span id="dot_6">&bull;</span>';
+    if ( dice ) {
+        dice.appendChild( this.node );
+    }
 }
 // Function: getRoll
 // Getter for <roll>
 Die.prototype.getRoll = function() {
-    return this.roll;
+    return this.rollValue;
 }
 // Function: setRoll
 // Setter for <roll>
 Die.prototype.setRoll = function( number ) {
-    this.roll = number;
+    this.rollValue = number;
+}
+// Function: roll
+// Get a random number for the roll, call setter & update classname.
+Die.prototype.roll = function() {
+    var newRoll = parseInt( ( Math.random() * 100 ) % 6, 10 );
+
+    this.setRoll( newRoll + 1 );
+    this.node.className = 'side_' + this.getRoll();
+}
+
+/******************************************************************************
+    Function: Card
+    Defines the Card object.
+******************************************************************************/
+var Card = function() {
+    // Variable: currentCard
+    // Tracks the current life card
+    this.currentCard = 0;
+    this.totalCards = 0;
+
+    this.setup();
+}
+// Function: getCurrentCard
+// Getter for <currentCard>
+Card.prototype.getCurrentCard = function() {
+    return this.currentCard;
+}
+// Function: setCurrentCard
+// Setter for <currentCard>
+Card.prototype.setCurrentCard = function( number ) {
+    this.currentCard = number;
+}
+Card.prototype.setup = function() {
+    var life_container = document.getElementById('life-cards');
+    this.life_cards = document.getElementById('life-card-list');
+    if ( this.life_cards ) {
+        this.totalCards = this.life_cards.getElementsByTagName('li').length;
+    }
+}
+// Function: draw
+// Get a random number for the current card, call setter & activate card.
+Card.prototype.draw = function() {
+    var newCardNumber = parseInt( ( Math.random() * 100 ) % this.totalCards, 10 ),
+        life_container = document.getElementById('life-cards'),
+        cards = this.life_cards.getElementsByTagName('li'),
+        activeClass = 'drawn',
+        newCard,
+        oldCard;
+
+    if ( newCardNumber == 0 ) {
+        this.draw();
+    } else {
+        if ( cards && cards.length ) {
+            newCard = cards[ newCardNumber ];
+            oldCard = cards[ this.getCurrentCard() ];
+            oldCard.className = oldCard.className.replace( ' ' + activeClass, '' );
+            this.setCurrentCard( newCardNumber );
+            newCard.className += ' ' + activeClass;
+        }
+    }
 }
